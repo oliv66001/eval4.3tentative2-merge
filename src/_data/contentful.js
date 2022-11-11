1d7fee00771fd24ac4196984d1f451c685825814
Olivier Pagé <oliv66001@gmail.com>
2022-11-11 19:10:14 +0100

    site fonctionel avant le changement index.html

--- src/_data/contentful.js
+++ src/_data/contentful.js
@@ -1,6 +1,18 @@
+const contentful = require("contentful");
+const client = contentful.createClient({
+  space: process.env.CTFL_SPACE,
+  accessToken: process.env.CTFL_ACCESSTOKEN
+});
+
 module.exports = function() {
-  return [
-    "user1",
-    "user2"
-  ];
+  return client.getEntries({ content_type: 'page', order: 'sys.createdAt' })
+  .then(function(response) {
+    const page = response.items
+    .map(function(page) {
+      page.fields.date= new Date(page.sys.updatedAt);
+      return page.fields;
+    });
+    return page;
+  })
+  .catch(console.error);
 };
